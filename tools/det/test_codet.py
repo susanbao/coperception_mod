5378129c6269d6116a639b7bef1bddf1a625b9da
Sanbao Su <sanbaosu@SanbaodeMacBook-Pro.local>
2022-08-30 00:08:04 -0400

    add save

--- tools/det/test_codet.py
+++ tools/det/test_codet.py
@@ -468,7 +468,9 @@ def main(args):
     mean_ap_local.append(mean_ap_local_average)
     covar_nll = eval_nll(det_results_all_local, annotations_all_local, scale_ranges=None, iou_thr=0.5)
     print(covar_nll)
-
+    npy_frame_file = os.path.join(model_save_path, "all_data_{args.nepoch}.npy")
+    det_res = {"det_results_frame": det_results_all_local, "annotations_frame": annotations_all_local}
+    np.save(npy_frame_file, det_res)
     mean_ap_local_average, _ = eval_map(
         det_results_all_local,
         annotations_all_local,
@@ -478,7 +480,7 @@ def main(args):
         logger=None,
     )
     mean_ap_local.append(mean_ap_local_average)
-    covar_nll = eval_nll(det_results_all_local, annotations_all_local, scale_ranges=None, iou_thr=0.5)
+    covar_nll = eval_nll(det_results_all_local, annotations_all_local, scale_ranges=None, iou_thr=0.7)
     print(covar_nll)
 
     print_and_write_log(
